4aa922e34429bfde6cec0dd131340bade9b43297
Maria Yadryshnikova <93837471+myadryshnikova@users.noreply.github.com>
2023-01-26 15:59:21 +0500

    ref: #861m9j5j4: cleanup code

--- picachu/config.py
+++ picachu/config.py
@@ -7,58 +7,34 @@ ENV = os.getenv('FLASK_ENV')
 DEBUG = ENV == 'development'
 
 s3_endpoint = os.getenv('S3_ENDPOINT')
+s3_access_key_id = os.getenv('S3_ACCESS_KEY_ID')
+s3_secret_access_key = os.getenv('S3_SECRET_ACCESS_KEY')
+s3_bucket_name = os.getenv('S3_BUCKET_NAME')
+s3_prefix = os.getenv('S3_PREFIX', default='')
+s3_use_ssl = os.getenv('S3_USE_SSL', default='true').lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup']
+
 if s3_endpoint is None:
     raise ValueError('You should specify S3_ENDPOINT environment variable to be able to connect to S3 bucket.')
-
-s3_access_key_id = os.getenv('S3_ACCESS_KEY_ID')
 if s3_access_key_id is None:
     raise ValueError('You should specify S3_ACCESS_KEY_ID environment variable to be able to connect to S3 bucket.')
-
-s3_secret_access_key = os.getenv('S3_SECRET_ACCESS_KEY')
 if s3_secret_access_key is None:
     raise ValueError('You should specify S3_SECRET_ACCESS_KEY environment variable to be able to connect to S3 bucket.')
-
-s3_bucket_name = os.getenv('S3_BUCKET_NAME')
 if s3_bucket_name is None:
     raise ValueError('You should specify S3_BUCKET_NAME environment variable to be able to connect to S3 bucket.')
 
-s3_prefix = os.getenv('S3_PREFIX', default='')
-
-s3_use_ssl = os.getenv('S3_USE_SSL', default='true').lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup']
 
 rabbitmq_host = os.getenv('RABBITMQ_HOST')
 rabbitmq_username = os.getenv('RABBITMQ_DEFAULT_USER')
 rabbitmq_password = os.getenv('RABBITMQ_DEFAULT_PASS')
 
-rabbitmq_requests_exchange_name = os.getenv('RABBITMQ_REQUESTS_EXCHANGE_NAME')
-
-if not rabbitmq_host:
+if rabbitmq_host is None:
     raise ValueError('You should specify RABBITMQ_HOST to be able to connect to RabbitMQ.')
-
-if not rabbitmq_username:
+if rabbitmq_username is None:
     raise ValueError('You should specify RABBITMQ_DEFAULT_USER to be able to connect to RabbitMQ.')
-
-if not rabbitmq_password:
+if rabbitmq_password is None:
     raise ValueError('You should specify RABBITMQ_DEFAULT_PASS to be able to connect to RabbitMQ.')
 
-if not rabbitmq_requests_exchange_name:
-    raise ValueError('You should specify RABBITMQ_REQUESTS_EXCHANGE_NAME to be able to connect to requests exchange.')
 
-class ExchangeNamesConfig:
-    def __init__(self):
-        self.rabbitmq_requests_exchange_name = rabbitmq_requests_exchange_name
-
-
-class RabbitMQConfigProvider:
-    @staticmethod
-    def get_config():
-        return (
-            rabbitmq_host,
-            rabbitmq_username,
-            rabbitmq_password,
-        )
-
-    @staticmethod
-    def get_exchange_names_config():
-        return ExchangeNamesConfig()
-
+rabbitmq_photo_for_models_exchange_name = os.getenv('RABBITMQ_PHOTO_FOR_MODELS_EXCHANGE_NAME')
+if rabbitmq_photo_for_models_exchange_name is None:
+    raise ValueError('You should specify RABBITMQ_PHOTO_FOR_MODELS_EXCHANGE_NAME to be able to connect to exchange.')
--- picachu/modules/photos/photos_routes.py
+++ picachu/modules/photos/photos_routes.py
@@ -4,9 +4,9 @@ import logging
 import imagehash
 import pika
 from PIL import Image
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
-from picachu.config import RabbitMQConfigProvider
+from picachu.config import rabbitmq_photo_for_models_exchange_name, rabbitmq_host, rabbitmq_username, rabbitmq_password
 from picachu.domain import Photo
 from picachu.modules.photos.commands.new_photo_command import NewPhotoCommand
 import io
@@ -16,25 +16,23 @@ from picachu.helpers.s3_paths import create_path_for_photo
 
 photos_blueprint = Blueprint('photos', __name__, url_prefix='/photos')
 
-(
-    rabbitmq_host,
-    rabbitmq_username,
-    rabbitmq_password,
-) = RabbitMQConfigProvider.get_config()
-
-parameters = ConnectionParameters(
+PARAMETERS = ConnectionParameters(
     host=rabbitmq_host,
     credentials=PlainCredentials(rabbitmq_username, rabbitmq_password),
 )
 
-exchange_name = RabbitMQConfigProvider.get_exchange_names_config().rabbitmq_requests_exchange_name
+EXCHANGE_NAME = rabbitmq_photo_for_models_exchange_name
+
 
 @photos_blueprint.route('/add', methods=['POST'])
 def add_photo_and_photo_labels():
     photo_bytes = request.get_data()
-    photo_s3_path = create_path_for_photo()
 
+    photo_s3_path = create_path_for_photo()
     photo_hash = str(imagehash.average_hash(Image.open(io.BytesIO(photo_bytes))))
+
+    photo_entity = Photo(photo_file_path_s3=photo_s3_path,
+                         hash=photo_hash)
 
     S3Helper().s3_upload_file(
         file_path_in_bucket=photo_s3_path,
@@ -42,44 +40,31 @@ def add_photo_and_photo_labels():
         public=True,
     )
 
-    photo_entity = Photo(photo_file_path_s3=photo_s3_path,
-                         hash=photo_hash)
-
     photo_id = NewPhotoCommand().create(photo_entity)
 
-    value = {
+    message_with_photo_parameters = {
         'photo_id': photo_id,
         'path_to_photo_in_s3': photo_s3_path,
     }
 
+    connection = pika.BlockingConnection(PARAMETERS)
+    channel = connection.channel()
+    channel.exchange_declare(exchange=EXCHANGE_NAME, exchange_type='fanout')
+    logging.warning('Exchange declared {0}'.format(EXCHANGE_NAME))
+
     try:
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=rabbitmq_host,
-                credentials=pika.credentials.PlainCredentials(rabbitmq_username, rabbitmq_password)
-            )
-        )
-        channel = connection.channel()
-
-        logging.warning('RabbitMQ exchange declaration {0}'.format(exchange_name))
-        channel.exchange_declare(exchange=exchange_name, exchange_type='fanout')
-
-        body_str = json.dumps(value)
-        body = body_str.encode('utf-8')
         channel.basic_publish(
-            exchange=exchange_name,
+            exchange=EXCHANGE_NAME,
             routing_key='',  # ignored by fanout exchange type
-            body=body,
+            body=json.dumps(message_with_photo_parameters).encode('utf-8'),
             properties=pika.BasicProperties(
-                delivery_mode=2,  # make message persistent
+                delivery_mode=2,
             )
         )
 
-        logging.warning('Model request sent: {0}'.format(body_str))
+        logging.warning('Model request sent: {0}'.format(message_with_photo_parameters))
     except Exception:
         logging.warning('Aborting...')
         connection.close()
 
-    return {
-        'photo_id': photo_id
-    }
+    return jsonify({'photo_id': photo_id})
