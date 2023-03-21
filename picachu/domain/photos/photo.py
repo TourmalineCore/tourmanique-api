185fcf581b0a57e160b408ffe4819280f79d06a3
Алексей <aaamegulya@gmail.com>
2023-03-21 16:22:31 +0500

    feat: #861mhnt65: connect photo to gallery

--- picachu/domain/photos/photo.py
+++ picachu/domain/photos/photo.py
@@ -1,5 +1,8 @@
 from picachu.domain.data_access_layer.db import db
 
+from datetime import datetime
+from sqlalchemy import DateTime
+
 
 class Photo(db.Model):
     __tablename__ = 'photos'
@@ -8,6 +11,7 @@ class Photo(db.Model):
     hash = db.Column(db.String(2048), nullable=True)
     photo_file_path_s3 = db.Column(db.String(2048), nullable=False)
     gallery_id = db.Column(db.BigInteger, db.ForeignKey('galleries.id'), nullable=False)
+    date_of_upload = db.Column(DateTime, default=datetime.utcnow, nullable=False)
 
     galleries = db.relationship('Gallery', back_populates='photos')
 
