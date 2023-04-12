0624147d4778e5c40b38827bcc119dee5f3cb3bf
Maria Yadryshnikova <93837471+myadryshnikova@users.noreply.github.com>
2023-04-12 08:58:13 +0500

    chore(jwt): #85zrwh58t: extend the life of a jwt user token by 30 days (#12)

--- picachu/modules/auth/auth_routes.py
+++ picachu/modules/auth/auth_routes.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
 
@@ -14,7 +16,8 @@ def log_in():
     if user_data != 'admin' or password_data != 'admin':
         return jsonify({'msg': 'Bad username or password'}), 401
 
-    access_token = create_access_token(identity=USER_ID)
+    access_token = create_access_token(identity=USER_ID,
+                                       expires_delta=datetime.timedelta(days=30))
 
     return jsonify({
         "accessToken": {
