793abfb66716cb43440c257ce8aa9e9431aeca94
Алексей <aaamegulya@gmail.com>
2023-04-18 17:30:26 +0500

    fix(jwt): #85zrwhzpb: add check оце шт .env and add JWT_TOKEN_KEY to .env.example

--- picachu/config/jwt_config.py
+++ picachu/config/jwt_config.py
@@ -1,3 +1,6 @@
 import os
 
 jwt_secret_token = os.getenv('JWT_SECRET_KEY')
+
+if jwt_secret_token is None:
+    raise ValueError('You should specify JWT_SECRET_KEY environment variable to be able to connect to JWT.')
