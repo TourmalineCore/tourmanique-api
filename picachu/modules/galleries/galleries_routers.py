74d37a16bfad53f147afbd0419bec9438003d323
alexeyadi <99860179+alexeyadi@users.noreply.github.com>
2023-04-19 09:56:20 +0500

    fix(gallery): #85zruenkj: add possibility to name the gallery when creating it (#14)

--- picachu/modules/galleries/galleries_routers.py
+++ picachu/modules/galleries/galleries_routers.py
@@ -3,13 +3,14 @@ from http import HTTPStatus
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
-from picachu.domain import Gallery, Photo
+from picachu.domain import Gallery
 
 from picachu.modules.auth.is_user_has_access import IsUserHasAccess
 
 from picachu.modules.galleries.commands.delete_gallery_command import DeleteGalleryCommand
 from picachu.modules.galleries.commands.new_gallery_command import NewGalleryCommand
 from picachu.modules.galleries.commands.update_gallery_command import UpdateGalleryCommand
+from picachu.modules.galleries.schemes.validation_gallery_name import ValidationGalleryName
 
 from picachu.modules.galleries.queries.get_gallery_query import GetGalleryQuery
 
@@ -22,11 +23,12 @@ galleries_blueprint = Blueprint('galleries', __name__, url_prefix='/galleries')
 @jwt_required()
 def add_gallery():
     current_user_id = get_jwt_identity()
+    validation_param = ValidationGalleryName(gallery_name=request.json.get('name'))
     if not IsUserHasAccess().to_service(current_user_id):
         return jsonify({'msg': 'Forbidden'}), HTTPStatus.FORBIDDEN
 
     gallery_entity = {
-                      'name': 'new gallery',
+                      'name': validation_param.gallery_name,
                       'user_id': current_user_id,
                       }
 
@@ -44,13 +46,13 @@ def add_gallery():
 @jwt_required()
 def rename_gallery(gallery_id):
     current_user_id = get_jwt_identity()
-    new_gallery_name = request.json.get('name')
+    validation_param = ValidationGalleryName(gallery_name=request.json.get('newName'))
     if not IsUserHasAccess().to_gallery(current_user_id, gallery_id):
         return jsonify({'msg': 'Forbidden'}), HTTPStatus.FORBIDDEN
     if not GetGalleryQuery.by_id(gallery_id):
         return jsonify({'msg': 'Not Found'}), HTTPStatus.NOT_FOUND
     try:
-        UpdateGalleryCommand().rename(new_gallery_name, gallery_id)
+        UpdateGalleryCommand().rename(validation_param.gallery_name, gallery_id)
         return jsonify({'msg': 'OK'}), HTTPStatus.OK
 
     except Exception as err:
