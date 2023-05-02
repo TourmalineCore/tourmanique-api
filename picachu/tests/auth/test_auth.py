d89719d393e0155a74f00230403954f755a538bc
Алексей <aaamegulya@gmail.com>
2023-05-02 17:02:22 +0500

    fix(test): #861mjpfwv: split function on two functions

--- picachu/tests/auth/test_auth.py
+++ picachu/tests/auth/test_auth.py
@@ -4,28 +4,33 @@ from flask import url_for
 from picachu.config.auth_config import auth_username, auth_password
 
 
+def test_log_in_happy_path(flask_app):
+    data = {
+        'login': auth_username,
+        'password': auth_password,
+    }
+
+    response = flask_app.post(url_for('api.auth.log_in'), json=data)
+
+    assert response.status_code == 202
+
+
 @pytest.mark.parametrize(
-    'login, password, valid',
+    'login, password',
     [
-        (auth_username, auth_password, True),
-        ('Admin', 'admin', False),
-        ('ADMIN', 'ADMIN', False),
-        ('admin2', 'admin2', False),
-        ('', 'admin2', False),
-        ('', '', False),
-        ('admin2', '', False),
+        ('Admin', 'admin'),
+        ('ADMIN', 'ADMIN'),
+        ('admin2', 'admin2'),
+        ('', 'admin2'),
+        ('', ''),
+        ('admin2', ''),
     ]
     )
-def test_log_in(login, password, valid, flask_app):
+def test_log_in_unhappy_path(login, password, flask_app):
     data = {
         'login': login,
         'password': password,
     }
 
     response = flask_app.post(url_for('api.auth.log_in'), json=data)
-
-    if valid:
-        assert response.status_code == 202
-    else:
-        assert response.status_code == 401
-
+    assert response.status_code == 401
