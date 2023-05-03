18e700f79d04498b7832055a901b937b4b09dc36
alexeyadi <99860179+alexeyadi@users.noreply.github.com>
2023-05-03 10:58:50 +0500

    fix(gallery): #861mpr5mr: block renaming gallery if it is deleted (#22)

--- picachu/modules/galleries/galleries_routers.py
+++ picachu/modules/galleries/galleries_routers.py
@@ -42,7 +42,7 @@ def add_gallery():
         return jsonify(str(err)), HTTPStatus.BAD_REQUEST
 
 
-@galleries_blueprint.route('/<int:gallery_id>/rename', methods=['POST'])
+@galleries_blueprint.route('/<int:gallery_id>/rename/', methods=['POST'])
 @jwt_required()
 def rename_gallery(gallery_id):
     current_user_id = get_jwt_identity()
