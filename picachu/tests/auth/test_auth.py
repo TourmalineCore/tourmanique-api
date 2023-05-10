a327524b996990770c2c3071b94da50ef4e4c20a
Алексей <aaamegulya@gmail.com>
2023-05-10 13:21:29 +0500

    fix(test): #861mjpfwv: rename functions, add validate_json_helper, correct test_log_in

--- picachu/tests/auth/test_auth.py
+++ picachu/tests/auth/test_auth.py
@@ -1,10 +1,13 @@
+from http import HTTPStatus
+
 import pytest
 from flask import url_for
 
 from picachu.config.auth_config import auth_username, auth_password
+from picachu.helpers.validate_json_helper import validate_json_schema
 
 
-def test_log_in_happy_path(flask_app):
+def test_log_in_with_the_correct_user_credentials(flask_app):
     data = {
         'login': auth_username,
         'password': auth_password,
@@ -12,25 +15,63 @@ def test_log_in_happy_path(flask_app):
 
     response = flask_app.post(url_for('api.auth.log_in'), json=data)
 
-    assert response.status_code == 202
+    schema = {
+      "type": "object",
+      "properties": {
+        "accessToken": {
+          "type": "object",
+          "properties": {
+            "value": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "value"
+          ]
+        }
+      },
+      "required": [
+        "accessToken"
+      ]
+    }
+
+    validate_response_schema = validate_json_schema(schema, response)
+
+    assert validate_response_schema is True
+    assert response.status_code == HTTPStatus.ACCEPTED
 
 
 @pytest.mark.parametrize(
     'login, password',
     [
-        ('Admin', 'admin'),
-        ('ADMIN', 'ADMIN'),
+        ('admin', 'ADMIN'),
+        ('admin2', 'admin'),
         ('admin2', 'admin2'),
-        ('', 'admin2'),
-        ('', ''),
-        ('admin2', ''),
     ]
     )
-def test_log_in_unhappy_path(login, password, flask_app):
+def test_log_in_with_the_incorrect_user_credentials(login, password, flask_app):
     data = {
         'login': login,
         'password': password,
     }
 
     response = flask_app.post(url_for('api.auth.log_in'), json=data)
-    assert response.status_code == 401
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+@pytest.mark.parametrize(
+    'login, password',
+    [
+        ('admin', ''),
+        ('', 'admin'),
+        ('', ''),
+    ]
+    )
+def test_log_in_with_empty_user_credentials(login, password, flask_app):
+    data = {
+        'login': login,
+        'password': password,
+    }
+
+    response = flask_app.post(url_for('api.auth.log_in'), json=data)
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
