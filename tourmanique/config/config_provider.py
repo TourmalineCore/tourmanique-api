be84d12ec45e1a3819dd22709fd57908b95241c5
Maria Yadryshnikova <93837471+myadryshnikova@users.noreply.github.com>
2025-05-26 00:13:01 +0500

    feat(local-env): change values.yaml and ci pipeline according to local-env approach (#36)
    * ci(build and push): add tc template for build and push image job
    
    * ci(comment): update docker-build-and-push.yml
    
    * ci(values): change values file name and cleanup
    
    * ci(fix): add nameOverride
    
    * ci(local-env): change values for testing in local env
    
    * feat(helmfile): add helmfile of service
    
    * fix(values): fix repository in values.yaml
    
    * chore(values): comment command and args
    
    * chore(values.yaml): add corrections after comparing with inner-circle-books-api
    
    * chore(values.yaml): correct port from 80 to 5000
    
    * chore(values.yaml): remove nameOverride
    
    * git: add .gitattributes to enforce LF line endings
    
    * fix(values.yaml): remove duplicate of lines
    
    * fix(values.yaml): remove unnecessary
    
    * fix(tag): add latest tag of image to fix compilation error
    
    * ci(extraConfigMapEnvVars): add extraDeploy to values.yaml for extraConfigMapEnvVars
    
    * ci(cleanup): change values.yaml
    
    * fix(postgres): add port to postgres connection string
    
    * ci(path): add postfix to path in values.yaml

--- tourmanique/config/config_provider.py
+++ tourmanique/config/config_provider.py
@@ -1,6 +1,6 @@
 from tourmanique.config.flask_config import env, debug
 from tourmanique.config.jwt_config import jwt_secret_key
-from tourmanique.config.postgres_config import postgres_database, postgres_host, postgres_password, postgres_username
+from tourmanique.config.postgres_config import postgres_database, postgres_host, postgres_password, postgres_username, postgres_port
 from tourmanique.domain.data_access_layer.engine import app_db_engine_provider
 
 
@@ -11,6 +11,7 @@ class ConfigProvider:
     SQLALCHEMY_DATABASE_URI = app_db_engine_provider.build_connection_string(
         database=postgres_database,
         host=postgres_host,
+        port=postgres_port,
         password=postgres_password,
         username=postgres_username,
     )
@@ -21,6 +22,7 @@ class TestConfigProvider(ConfigProvider):
     SQLALCHEMY_DATABASE_URI = app_db_engine_provider.build_connection_string(
         database='test_db',
         host='test-db',
+        port='5432',
         password='postgres',
         username='postgres',
     )
